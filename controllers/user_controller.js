8355495d9f114ecb43e4cce0dfa6934e69e47548
tharukeRajapaksha123 <darksparrow156@gmail.com>
2023-01-20 06:55:49 +0530

    ADD:notifications main functionality added

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const admin = require("firebase-admin")
+var FCM = require('fcm-node');
 const credentials = require("../serviceAccountKeys.json");
 
 const router = express.Router();
@@ -84,10 +85,10 @@ router.get("/get-user/:username", async (req, res) => {
         .get()
         .then((snapshot) => {
             let result = [];
-            for (var re of snapshot.docs){
+            for (var re of snapshot.docs) {
                 result.push(re.data())
             }
-            return res.status(200).json({ "users": result});
+            return res.status(200).json({ "users": result });
         })
         .catch(error => {
             return res.status(500).json({ "message": `get user information failed ${error}` })
@@ -95,26 +96,79 @@ router.get("/get-user/:username", async (req, res) => {
 })
 
 
-router.put('/edit-user', (req, res) => {
-    const uid = req.body.uid;
+router.put('/edit-user/:uid', async (req, res) => {
+    const uid = req.params.uid;
     const data = req.body.data;
-    db.collection("users").doc(uid).update(data)
-      .then(() => {
-          return res.status(201).json({ "message": "user updated succesfully" });
-      }).catch(err => {
-          console.log(`update firestore data failed ${err}`);
-          return res.status(500).json({ "message": `data update failed ${err}` });
-      });
-  });
+    return await db.collection("users").doc(uid).update(data)
+        .then(() => {
+            return res.status(201).json({ "message": "user updated succesfully" });
+        }).catch(err => {
+            console.log(`update firestore data failed ${err}`);
+            return res.status(500).json({ "message": `data update failed ${err}` });
+        });
+});
 
-  router.delete('/delete-user', (req, res) => {
-    const uid = req.body.uid;
-    db.collection('users').doc(uid).delete()
-      .then(() => {
-          return res.status(201).json({ "message": "user deleted succesfully" });
-      }).catch(err => {
-          console.log(`deleting firestore data failed ${err}`);
-          return res.status(501).json({ "message": `data deletion failed ${err}` });
-      });
-  });
+router.delete('/delete-user/:uid', async (req, res) => {
+    const uid = req.params.uid;
+    return await db.collection('users').doc(uid).delete()
+        .then(async () => {
+            await admin.auth().deleteUser(uid);
+            return res.status(201).json({ "message": "user deleted succesfully" });
+        }).catch(err => {
+            console.log(`deleting firestore data failed ${err}`);
+            return res.status(501).json({ "message": `data deletion failed ${err}` });
+        });
+});
+
+router.put("/block-user/:uid", async (req, res) => {
+    const uid = req.params.uid;
+    return await db.collection("users").doc(uid).update({ "disabled": true }).then((va) => {
+        return res.status(201).json({ "message": "user blocked succesfully" })
+    }).catch(error => {
+        return res.status(500).json({ "message": `user blocked failed ${error}` })
+    })
+})
+router.put("/unblock-user/:uid", async (req, res) => {
+    const uid = req.params.uid;
+    return await db.collection("users").doc(uid).update({ "disabled": false }).then((va) => {
+        return res.status(201).json({ "message": "user unblocked succesfully" })
+    }).catch(error => {
+        return res.status(500).json({ "message": `user un   blocked failed ${error}` })
+    })
+})
+
+router.get("/send-notification-all", (req, res) => {
+    
+    const key = "cuFI4OvaTLSdDpsoMvb1JD:APA91bEvx2Z-_qcy-_9egaE5bP0tF7kwiZbqa8e0z0RzjGowea0hYaNOI1dUd2DCP5UtTpVcGvibMa2ba8jREeaWsifzYYre9R9p0RtHQTaiaNmT3Br0IIxDVPHBvS1gvGYJfBOV5o1F";
+
+    var serverKey = key //put your server key here
+    var fcm = new FCM(serverKey);
+
+    var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+        to: 'registration_token',
+        collapse_key: 'your_collapse_key',
+
+        notification: {
+            title: 'Title of your push notification',
+            body: 'Body of your push notification'
+        },
+
+        data: {  //you can send only notification or only data(or include both)
+            my_key: 'my value',
+            my_another_key: 'my another value'
+        }
+    };
+
+    fcm.send(message, function (err, response) {
+        if (err) {
+            console.log("Something has gone wrong!");
+        } else {
+            console.log("Successfully sent with response: ", response);
+        }
+    });
+
+    return res.status(200).json({"message" :"notified succesfully"})
+})
+
+
 module.exports = router
