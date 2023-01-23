f69765530717df1dca1cf3e612d7abab5f602948
Gayashi <herath.gayashie@gmail.com>
2023-01-23 14:24:18 +0530

    new

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -225,4 +225,34 @@ router.put('/unassign-courses-from-group/:groupId', async (req, res) => {
     });
 });
 
+//Add Assessment to a module
+router.post('/add-module-assessment', async (req, res) => {
+    try {
+        const module = req.body;
+        const db = admin.firestore();
+        await db.collection('modules').doc(module.id).set({
+            'assessment_id': module.assessmentId 
+        });
+        res.status(200).send({ success: true });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ success: false, error });
+    }
+});
+
+//Add Exam to a module
+router.post('/add-module-exam', async (req, res) => {
+    try {
+        const module = req.body;
+        const db = admin.firestore();
+        await db.collection('modules').doc(module.id).set({
+            'exam_id': module.examId 
+        });
+        res.status(200).send({ success: true });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ success: false, error });
+    }
+});
+
 module.exports = router
