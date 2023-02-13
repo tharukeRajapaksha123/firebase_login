fea101477754ea8582cda19d9af0b4a9d4b6d5b9
Gimhan Tharuke Rajapaksha <44710949+tharukeRajapaksha123@users.noreply.github.com>
2023-02-13 05:35:53 +0530

    keys removed

--- firebase_config.js
+++ firebase_config.js
@@ -1,13 +1,7 @@
 const firebase = require('firebase');
 
 const firebaseConfig = {
-   apiKey: "AIzaSyDomu7XkX48lxMC5_RNZMCj-wfjAY1UtBo",
-   authDomain: "dgmentor-93b90.firebaseapp.com",
-   projectId: "dgmentor-93b90",
-   storageBucket: "dgmentor-93b90.appspot.com",
-   messagingSenderId: "711555357479",
-   appId: "1:711555357479:web:7bd6c2a4d26c80c77ba702",
-   measurementId: "G-35FV6G71KY"
+
 };
 
 //initialize firebase app 
